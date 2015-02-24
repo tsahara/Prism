5316c0bea2652e9a1a4a156b2d37893567df550d
Tomoyuki Sahara <sahara@caddr.net>
2015-02-24 10:31:11 +0900

    Initial Commit

---
+++ Prism/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Prism
+//
+//  Created by Tomoyuki Sahara on 2/24/15.
+//  Copyright (c) 2015 Tomoyuki Sahara. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
