a0268c93c18ff25fa888ad2c93d15b4d37e096e3
Tomoyuki Sahara <sahara@caddr.net>
2015-09-06 20:25:09 +0900

    parse a packet.

--- Prism/Packet.swift
+++ Prism/Packet.swift
@@ -9,10 +9,64 @@
 import Foundation
 
 class Packet {
-//    let data: NSData
-//    let original_length: Int
-//    let captured_length: Int
-//    let timestamp: UInt64
+    let timestamp: NSDate
+    let original_length: Int
+    let captured_length: Int
+    let data: NSData
     
+    init(timestamp: NSDate, original_length: Int, captured_length: Int, data: NSData) {
+        self.timestamp = timestamp
+        self.original_length = original_length
+        self.captured_length = captured_length
+        self.data = data
+    }
     
+    class func parseText(text: String) -> Packet? {
+        var timestamp: NSDate?
+        var buf = [UInt8](count: 2000, repeatedValue: 0)
+        var idx = 0
+
+        var now = time(nil)
+        let tm_now = localtime(&now).memory
+
+        let re_summary = NSRegularExpression(pattern: "^(\\d{2}):(\\d{2}):(\\d{2})\\.(\\d+) ", options: nil, error: nil)!
+        let re_bytes = NSRegularExpression(pattern: "^ *0x(?:[0-9a-fA-F]{4}): ((?: [0-9a-fA-F]+)+)", options: nil, error: nil)!
+        
+        NSString(string: text).enumerateLinesUsingBlock {
+            line, _ in
+            let nsline = line as NSString
+
+            if let m = re_summary.firstMatchInString(line, options: nil, range: NSRange(location: 0, length: count(line))) {
+                var tm = tm_now
+                tm.tm_hour = Int32(nsline.substringWithRange(m.rangeAtIndex(1)).toInt()!)
+                tm.tm_min  = Int32(nsline.substringWithRange(m.rangeAtIndex(2)).toInt()!)
+                tm.tm_sec  = Int32(nsline.substringWithRange(m.rangeAtIndex(3)).toInt()!)
+                let sec = mktime(&tm)
+                let subsec = UInt64(nsline.substringWithRange(m.rangeAtIndex(4)).toInt()!)
+                timestamp = NSDate(timeIntervalSince1970: NSTimeInterval(mktime(&tm)))
+            }
+
+            if let m = re_bytes.firstMatchInString(line, options: nil, range: NSRange(location: 0, length: count(line))) {
+                for s in nsline.substringWithRange(m.rangeAtIndex(1)).componentsSeparatedByString(" ") {
+                    if count(s) == 4 {
+                        let word = strtoul(s, nil, 16)
+                        buf[idx]   = UInt8(word >> 8)
+                        buf[idx+1] = UInt8(word & 0xff)
+                        idx += 2
+                    } else if count(s) == 2 {
+                        let word = strtoul(s, nil, 16)
+                        buf[idx] = UInt8(word & 0xff)
+                        idx += 1
+                    }
+                }
+
+            }
+        }
+        let data = NSData(bytes: buf, length: idx)
+        if timestamp != nil {
+            return Packet(timestamp: timestamp!, original_length: data.length, captured_length: data.length, data: data)
+        } else {
+            return nil
+        }
+    }
 }
--- PrismTests/PacketTests.swift
+++ PrismTests/PacketTests.swift
@@ -23,14 +23,15 @@ class PacketTests: XCTestCase {
 
     func testParseText1() {
         let text_icmp6_echo =
-        "reading from file Data/icmp6-lo.pcap, link-type NULL (BSD loopback)\n" +
             "21:31:01.116717 IP6 ::1 > ::1: ICMP6, echo request, seq 0, length 16\n" +
             "   0x0000:  6000 a182 0010 3a40 0000 0000 0000 0000  `.....:@........\n" +
             "   0x0010:  0000 0000 0000 0001 0000 0000 0000 0000  ................\n" +
             "   0x0020:  0000 0000 0000 0001 8000 99b9 4d46 0000  ............MF..\n" +
         "   0x0030:  53df 7d05 0001 c7cd                      S.}.....\n"
-       
-        XCTAssert(true, "Pass")
+        let pkt = Packet.parseText(text_icmp6_echo)
+
+        XCTAssertNotNil(pkt, "Pass")
+        XCTAssertEqual(pkt!.captured_length, 56)
     }
 
     func testPerformanceExample() {
