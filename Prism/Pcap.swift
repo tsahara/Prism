6a3f26ca451a810537f0074b7af3a7adf33fdc20
Tomoyuki Sahara <sahara@caddr.net>
2015-09-07 22:56:13 +0900

    Pcap is a superclass of pcap, pcap-ng, ...

---
+++ Prism/Pcap.swift
@@ -0,0 +1,13 @@
+//
+//  Pcap.swift
+//  Prism
+//
+//  Created by Tomoyuki Sahara on 9/7/15.
+//  Copyright (c) 2015 Tomoyuki Sahara. All rights reserved.
+//
+
+import Foundation
+
+class Pcap {
+    var packets: [Packet] = []
+}
