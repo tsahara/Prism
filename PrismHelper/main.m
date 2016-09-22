48af7109deaca30d0a8700db3aa83ebefea3e247
Tomoyuki Sahara <sahara@caddr.net>
2016-09-23 07:55:34 +0900

    added to git repository.
    I don't know why Xcode refused to add this file to git repository.
    I had to run "git" command manually to add it.

---
+++ PrismHelper/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PrismHelper
+//
+//  Created by Tomoyuki Sahara on 2/7/16.
+//  Copyright © 2016 Tomoyuki Sahara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
