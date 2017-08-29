70d17fe30c755a837575d4ed67adfc009c8ac453
Tomoyuki Sahara <sahara@caddr.net>
2017-08-29 23:57:11 +0900

    wip

---
+++ Prism/SessionTable.swift
@@ -0,0 +1,19 @@
+//
+//  SessionTable.swift
+//  Prism
+//
+//  Created by Tomoyuki Sahara on 2017/08/29.
+//  Copyright © 2017 Tomoyuki Sahara. All rights reserved.
+//
+
+import Foundation
+
+class SessionTable {
+    var sessions: [Session] = []
+}
+
+class Session {
+}
+
+class TCPSession : Session {
+}
